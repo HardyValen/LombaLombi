535657fe5427b977e5fffec0c4e7604c458ccb24
Hardy Valenthio <34004631+HardyValen@users.noreply.github.com>
2018-09-19 12:08:48 +0700

    Add files via upload
    ##V.0.1.0 -- Initial
    Baru konten saja (index dan about)

---
+++ impulse/about.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+    <?php include 'php/head.php'; ?>
+    <title>Document</title>
+</head>
+
+<section class='intro'>
+    <div class='content'>
+        <div class='row'>
+            <div class='offset-md-2 col-md-8'>
+                <h1 class='merriweather-900i small-caps'>LATAR BELAKANG</h1>
+                <p class='merriweather line-height-6'>
+                    Indonesia merupakan negara yang memiliki potensi sumber daya alam yang besar seperti sumber energi, material, hayati dan lain-lain. Di sisi lain negara ini masih mengalami keterbelakangan di berbagai bidang, atau sering disebut dengan fenomena paradox of plenty atau resource curse. Berdasarkan data United Nations Development Programme, Indonesia mempunyai index pembangunan manusia sebesar 0,689 atau peringkat 113 dunia. Hal ini menunjukkan kualitas dan potensi SDA yang besar tidak berbanding lurus dengan kualitas dari penduduk Indonesia.
+                </p>
+                <p class='merriweather line-height-6'>
+                    Sementara itu, Indonesia akan memperoleh bonus demografi pada tahun 2030 . Dengan demikian setidaknya dua dekade mendatang menjadi momen krusial untuk mewujudkan cita-cita besar Indonesia, khususnya dalam revolusi industri secara besar. Dalam hal ini, peran penting dimainkan oleh pemuda, khususnya siswa SMA dan Mahasiswa di bidang science dan engineering.
+                </p>
+                <p class='merriweather line-height-6'>
+                    Menyadari hal-hal di atas, IMPULSE sebagai acara dari Himpunan Mahasiswa Elektronik (HME) ITB kembali hadir. Kompetisi ini bertujuan untuk memberi wadah bagi generasi muda dalam menuangkan ide-ide inovatif dan kritis terhadap kondisi di sekitarnya. Melalui kegiatan ini, kami berharap generasi muda terdorong untuk bergerak membangun bangsa, khususnya di era revolusi industri modern ini.
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class='intro'>
+    <div class='content'>
+        
+        <div class='row'>
+            <div class='offset-md-2 col-md-8'>
+                <h1 class='merriweather-900i small-caps'>TUJUAN</h1>
+                <p class='merriweather line-height-6'>
+                    Wadah untuk berkompetisi dan melatih kemampuan pemuda baik soft skills maupun technical knowledge khususnya dalam bidang teknologi dengan secara kritis dan inovatif menjawab tantangan masa depan.
+                </p>
+                <p class='merriweather line-height-6'>
+                    Wadah interaktif antara pemuda dan perusahaan untuk mencari solusi khususnya di bidang industri dan teknologi.
+                </p>
+                <p class='merriweather line-height-6'>
+                    Wadah untuk bertukar gagasan dan pengetahuan antar pemuda dari berbagai bidang pendidikan.
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+
+</body>
+</html>
